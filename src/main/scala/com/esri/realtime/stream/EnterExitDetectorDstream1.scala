97a64a3b1529e8e6c9f33542ddb59c79bd577f02
Vlad Plechnoy <vplechnoy@esri.com>
2019-04-22 16:35:39 -0700

    StatefulRealTimeStream

--- src/main/scala/com/esri/realtime/stream/EnterExitDetectorDstream1.scala
+++ src/main/scala/com/esri/realtime/stream/EnterExitDetectorDstream1.scala
@@ -1,35 +1,54 @@
 package com.esri.realtime.stream
 
-import com.esri.arcgis.st.{Feature, FeatureSchema}
+import com.esri.arcgis.st.{Feature, FeatureSchema, HasFeatureSchema}
 import com.esri.core.geometry.Point
 import com.esri.realtime.core.execution.ExecutionContextHolder
 import com.esri.realtime.core.featureFunctions
 import com.esri.realtime.messaging.kafka.consumer.Kafka
 import org.apache.spark.groupon.metrics.UserMetricsSystem
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream.DStream
+import scala.annotation.meta.param
 
-object EnterExitDetectorDstream {
+object EnterExitDetectorDstream1 {
 
-  case class FeatureTrackUpdater(trackPurger: FeatureTrackPurger)(implicit val featureSchema: FeatureSchema) {
+  case class StatefulRealTimeStream(@(transient @param) parent: DStream[Feature], @(transient @param) states: DStream[(String, FeatureTrack)], featureSchema: FeatureSchema) extends HasFeatureSchema
 
-    def update(trackId: String, featureOpt: Option[Feature], state: State[FeatureTrack]): FeatureTrack = {
-      val featureTrack = state.getOption.getOrElse(FeatureTrack(trackPurger))
-      featureOpt map {
-        feature: Feature => featureTrack add feature
-      }
-      state.update(featureTrack)
-      featureTrack
+  object StatefulRealTimeStream {
+
+    def apply(dstream: DStream[Feature], featureSchema: FeatureSchema, statePurger: FeatureTrackPurger): StatefulRealTimeStream = {
+
+      val featuresWithTrackIds = dstream.map(feature => (feature.trackId(featureSchema), feature))
+
+      // We'll define our state using our trackStateFunc function called updateFeatureTrack above, and also specify a session timeout value of 30 minutes.
+      val stateSpec = StateSpec.function((trackId: String, featureOpt: Option[Feature], state: State[FeatureTrack]) => {
+        val featureTrack = state.getOption.getOrElse(FeatureTrack(statePurger)(featureSchema))
+        featureOpt map {
+          feature: Feature => featureTrack add feature
+        }
+        state.update(featureTrack)
+        featureTrack
+      }) //.timeout(Minutes(30)) ???
+
+      // Process features through StateSpec to update the state
+      val featuresWithState = featuresWithTrackIds.mapWithState(stateSpec)
+
+      // Take a snapshot of the current state so we can look at it
+      val featureTrackStateStream: DStream[(String, FeatureTrack)] = featuresWithState.stateSnapshots()
+
+      StatefulRealTimeStream(dstream, featureTrackStateStream, featureSchema)
     }
   }
 
   def main(args: Array[String]): Unit = {
 
     if (args.length != 3) {
-      System.err.println("Usage: EnterExitDetectorDstream <zkQuorum> <bootstrap servers> <kafka topic>")
+      System.err.println("Usage: EnterExitDetectorDstream1 <zkQuorum> <bootstrap servers> <kafka topic>")
       System.exit(1)
     }
 
-    implicit val featureSchema: FeatureSchema = FeatureSchema(
+    val featureSchema: FeatureSchema = FeatureSchema(
       """
         |{
         |  "attributes": [
@@ -109,13 +128,18 @@ object EnterExitDetectorDstream {
         |}
       """.stripMargin
     )
-    val featureTrackUpdater = FeatureTrackUpdater(MaxFeaturesPerTrackPurger(2))
 
-    // Create the context with a 1 second batch size
-    val ssc = new StreamingContext("local[4]", "EnterExitDetectorDstream", Seconds(1))
-    ssc.checkpoint("/checkpoint/")
+    // Get or create the context with a 1 second batch size
+    val ssc = StreamingContext.getOrCreate(checkpointPath = "/checkpoint/",
+      creatingFunc = () => {
+        val ssc = new StreamingContext("local[4]", "EnterExitDetectorDstream1", Seconds(1))
+        ssc.checkpoint("/checkpoint/")
+        ssc
+      },
+      createOnError = true
+    )
     UserMetricsSystem.initialize(ssc.sparkContext)
-    ExecutionContextHolder.init("EnterExitDetector", 1000, ssc.sparkContext)
+    ExecutionContextHolder.init("EnterExitDetectorDstream1", 1000, ssc.sparkContext)
 
     val gatewayConsumer = Kafka.definition.newInstance(
       Map(
@@ -126,25 +150,16 @@ object EnterExitDetectorDstream {
       )
     )
 
-    // We'll define our state using our trackStateFunc function called updateFeatureTrack above, and also specify a session timeout value of 30 minutes.
-    val stateSpec = StateSpec.function(featureTrackUpdater.update _)//.timeout(Minutes(30))
+    val gatewayStream = gatewayConsumer.getDStream
 
-    val dstream = gatewayConsumer.getDStream
+    val statefulStream = StatefulRealTimeStream(gatewayStream, featureSchema, MaxFeaturesPerTrackPurger(2))
 
-    val featuresWithTrackIds = dstream.map(feature => (feature.trackId(featureSchema), feature))
-
-    // Process features through StateSpec to update the state
-    val featuresWithState = featuresWithTrackIds.mapWithState(stateSpec)
-
-    // Take a snapshot of the current state so we can look at it
-    val stateSnapshotStream = featuresWithState.stateSnapshots()
+    val sqlContext: SQLContext = SQLContextSingleton.getInstance(statefulStream.parent.context.sparkContext)
+    import sqlContext.implicits._
 
     // Process each RDD from each batch as it comes in
-    stateSnapshotStream.foreachRDD((rdd, time) => {
-      // Get the singleton instance of SQLContext
-      val sqlContext = SQLContextSingleton.getInstance(rdd.context)
-      import sqlContext.implicits._
-
+    val name = "gateway"
+    statefulStream.states.foreachRDD((rdd, time) => {
       val df = rdd.map {
         case (trackId, track) =>
           track.latest match {
@@ -168,10 +183,10 @@ object EnterExitDetectorDstream {
       }.toDF("trackId", "latestTime", "latestPosition")
 
       // Create a SQL table from this DataFrame
-      df.createOrReplaceTempView("featureTracks")
+      df.createOrReplaceTempView(name)
 
       // Dump out the results - you can do any SQL you want here.
-      val featureTracksDataFrame = sqlContext.sql("select * from featureTracks")
+      val featureTracksDataFrame = sqlContext.sql(s"select * from $name")
       println(s"========= $time =========")
       featureTracksDataFrame.show()
     })
